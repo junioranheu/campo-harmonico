daab2076f86a66909b1ddeaf66510eb7fa316ef0
junioranheu <junioranheu@gmail.com>
2022-07-25 19:26:21 -0300

    continuar desenvolvendo o jogo de Campo Harmonico

--- CamposHarmonicos/Program.cs
+++ CamposHarmonicos/Program.cs
@@ -7,14 +7,70 @@ List<string> campoHarmonicoF = new(new string[] { "F", "Gm", "Am", "A#", "C", "D
 List<string> campoHarmonicoG = new(new string[] { "G", "Am", "Bm", "C", "D", "Em" });
 List<List<string>> listaCamposHarmonicos = new(new List<string>[] { campoHarmonicoA, campoHarmonicoB, campoHarmonicoC, campoHarmonicoD, campoHarmonicoE, campoHarmonicoF, campoHarmonicoG });
 
+static Tuple<int, string> SelecionarNota(List<string> campo)
+{
+    // Randomizar;
+    Random? random = new();
+    int index = 0;
+    string nota = "";
+    bool isOk = false;
+
+    do
+    {
+        index = random.Next(campo.Count);
+        nota = campo[index];
+
+        if (nota != "")
+        {
+            isOk = true;
+        }
+    } while (!isOk);
+
+    // Atualizar valor do item na lista;
+    campo[index] = "";
+
+    // Verificar texto;
+    string texto = SelecionarTexto(index);
+
+    return Tuple.Create(index, texto);
+}
+
+static string SelecionarTexto(int index)
+{
+    if (index == 2)
+    {
+        return "terceira";
+    }
+    else if (index == 3)
+    {
+        return "quarta";
+    }
+    else if (index == 4)
+    {
+        return "quinta";
+    }
+    else if (index == 5)
+    {
+        return "sexta";
+    }
+
+    return "primeira";
+}
+
 foreach (var campo in listaCamposHarmonicos)
 {
-    Console.WriteLine($"Campo harmonico de {campo[0]}");
+    string tom = campo[0];
+    campo.RemoveAt(0);
+    Console.WriteLine($"\nCampo harmonico de {tom}");
 
-    // foreach (var nota in campo.OrderBy(a => Guid.NewGuid()).ToList())
-    foreach (var nota in campo)
+    foreach (var nota in campo.ToList())
     {
-        Console.WriteLine(nota);
+        Tuple<int, string> itemSelecionado = SelecionarNota(campo);
+
+        if (campo.Count > 0)
+        {
+            Console.WriteLine($"Qual é a {itemSelecionado.Item2} de {tom}?");
+        }
     }
 }
 
